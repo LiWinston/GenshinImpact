7676c179847a23b675c710b53dbc181099867811
Seeker <yongchunl@student.unimelb.edu.au>
2023-08-09 13:22:52 +1000

    Controllable Health & Energy bar
    A health bar and energy bar component based on free resources, displayed at a fixed position in the UI. Their values can be controlled by the unit's health script or exposed interface variables.

---
+++ Assets/Health.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
+
+    public float maxEnergy = 100f;
+    public float currentEnergy = 100f;
+
+    public GameObject healthBarObject;
+    public GameObject energyBarObject;
+
+    private Image healthBar;
+    private Image energyBar;
+
+    private void Start()
+    {
+        healthBar = healthBarObject.GetComponent<Image>();
+        energyBar = energyBarObject.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        currentEnergy = Mathf.Clamp(currentEnergy, 0f, maxEnergy);
+
+        healthBar.fillAmount = currentHealth / maxHealth;
+        energyBar.fillAmount = currentEnergy / maxEnergy;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+    }
+
+    public void ConsumeEnergy(float amount)
+    {
+        currentEnergy -= amount;
+    }
+
+    public void RestoreEnergy(float amount)
+    {
+        currentEnergy += amount;
+    }
+}
