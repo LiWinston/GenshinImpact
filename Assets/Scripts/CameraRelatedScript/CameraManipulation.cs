63d08cdd05acb169ad1b49b131151bb4d9c4264a
WL <yongchunl@student.unimelb.edu.au>
2023-09-18 02:52:38 +1000

    Set WALL Layer + Handle Obstacled Camera

--- Assets/Scripts/CameraRelatedScript/CameraManipulation.cs
+++ Assets/Scripts/CameraRelatedScript/CameraManipulation.cs
@@ -8,11 +8,16 @@ namespace CameraRelatedScript
         [SerializeField]
         private Transform attachedCamera;
         
-        [SerializeField]GameObject viewPoint;
+        [SerializeField] GameObject viewPoint;
+        [SerializeField] GameObject FPCamera; // 备用相机
+        [SerializeField] private LayerMask wallLayer; // 墙体的Layer
 
-        // private PlayerController playerController; // Automatically get the PlayerController component
         [SerializeField] private float forwardOffset;
         [SerializeField] private float upwardOffset;
+        [SerializeField] private float FPforwardOffset;
+        [SerializeField] private float FPupwardOffset;
+        private float transitionSpeed = 1.0f; // 调整过渡速度
+        private bool obstacleInWay = false; // 添加一个标志来表示是否有障碍物
 
         private void Start()
         {
@@ -21,23 +26,48 @@ namespace CameraRelatedScript
                 Debug.LogError("viewPoint not found!");
             }
             
-            // playerController = GetComponent<PlayerController>();
-            // if (playerController == null)
-            // {
-            //     Debug.LogWarning("PlayerController component not found on the same GameObject!");
-            // }
+            if (FPCamera == null)
+            {
+                Debug.LogError("FPCamera not found!");
+            }
         }
 
         private void LateUpdate()
         {
-            Vector3 newPosition = viewPoint.transform.position; // Use the eye position as the base position
-            
-            
-            // Optionally, you can add an offset forward and/or upward
-            newPosition += transform.forward * forwardOffset;
-            newPosition += transform.up * upwardOffset;
+            Vector3 newPosition = viewPoint.transform.position;
 
-            attachedCamera.position = newPosition;
+            // 使用射线检测来避免相机被墙体阻挡
+            RaycastHit hit;
+            Vector3 cameraToViewPoint = viewPoint.transform.position - attachedCamera.position;
+
+            if (Physics.Raycast(attachedCamera.position, cameraToViewPoint, out hit, cameraToViewPoint.magnitude, wallLayer))
+            {
+                if (!obstacleInWay)
+                {
+                    // 切换到备用相机
+                    FPCamera.SetActive(true);
+                    attachedCamera.gameObject.SetActive(false);
+                    obstacleInWay = true;
+                }
+
+                // 使用备用相机的位置
+                Vector3 newFPPosition = newPosition + transform.forward * FPforwardOffset + transform.up * FPupwardOffset;
+                FPCamera.transform.position = Vector3.Lerp(FPCamera.transform.position, newFPPosition, Time.deltaTime * transitionSpeed);
+            }
+            else
+            {
+                if (obstacleInWay)
+                {
+                    // 切换回原相机
+                    FPCamera.SetActive(false);
+                    attachedCamera.gameObject.SetActive(true);
+                    obstacleInWay = false;
+                }
+
+                newPosition += transform.forward * forwardOffset + transform.up * upwardOffset;
+                // 平滑过渡到新位置
+                attachedCamera.position = Vector3.Lerp(attachedCamera.position, newPosition, Time.deltaTime * transitionSpeed);
+            }
         }
     }
 }
