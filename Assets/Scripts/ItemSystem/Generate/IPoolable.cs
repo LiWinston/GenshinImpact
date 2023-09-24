da222793cb6d541e5c11fc3d02a869628f53ba27
WL <yongchunl@student.unimelb.edu.au>
2023-09-24 12:09:56 +1000

    Updated Object Pool implementation
    更新对象池实现 修复已重生对象仍受原存活计时支配问题

--- Assets/Scripts/ItemSystem/Generate/IPoolable.cs
+++ Assets/Scripts/ItemSystem/Generate/IPoolable.cs
@@ -1,9 +1,26 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.Pool;
 
 public interface IPoolable
 {
-    void SetPool(ObjectPool<GameObject> pool);
-    void actionOnGet();
-    void actionOnRelease();
+    public ObjectPool<GameObject> ThisPool { get; set; }
+    bool IsExisting { get; set; }
+
+    void SetPool(ObjectPool<GameObject> pool)
+    {
+        ThisPool = pool;
+    }
+
+    public void actionOnGet()
+    {
+        IsExisting = true;
+    }
+
+    void actionOnRelease()
+    {
+        IsExisting = false;
+    }
+
+    void Release();
 }
