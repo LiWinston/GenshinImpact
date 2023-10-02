a982d7350c4bd689b50c45d3daba7fe276875640
yuxinr1 <yuxinr1@student.unimelb.edu.au>
2023-10-02 18:47:00 +1100

    结束界面

---
+++ Assets/Scripts/UI/Bottons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Bottons : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
