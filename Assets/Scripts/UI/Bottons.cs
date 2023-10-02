06ab8ebd25c26d23c3538dd0aa4bd0da6c32ce35
WL <yongchunl@student.unimelb.edu.au>
2023-10-02 19:30:28 +1100

    修复：结束界面锁定鼠标不能点击

--- Assets/Scripts/UI/Bottons.cs
+++ Assets/Scripts/UI/Bottons.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Bottons : MonoBehaviour
 {
+    public void Start(){
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene("StartScene");
