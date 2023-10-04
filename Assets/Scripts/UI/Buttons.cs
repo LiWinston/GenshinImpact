c4aadf98b5706a610ae1da925719c10ef6381cb4
rongroy <rozhao1@student.unimelb.edu.au>
2023-10-04 16:10:54 +1100

    StartMenu

--- Assets/Scripts/UI/Buttons.cs
+++ Assets/Scripts/UI/Buttons.cs
@@ -3,11 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Bottons : MonoBehaviour
+public class Buttons : MonoBehaviour
 {
     public void Start(){
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+    public void QuitGame0()
+    {
+        Application.Quit();
     }
 
     public void RestartGame()
