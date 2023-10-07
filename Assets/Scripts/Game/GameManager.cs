a7b748cb542260aa52f9e4311ac72169fb02671d
WL <yongchunl@student.unimelb.edu.au>
2023-10-07 16:13:25 +1100

    修复四分钟转向问题

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -11,7 +11,7 @@ namespace Game
     {
         public Text timerText;
         private float startTime;
-        private bool gameEnded = false;
+        private bool gameEnded;
         public Transform bossRoom;
         public Transform lookat;
         private bool isFinalBattle = false;
@@ -21,8 +21,10 @@ namespace Game
 
         private void Start()
         {
+            gameEnded = false;
+            ElapsedTime = 0f;
             // 获取当前场景加载的时间
-            startTime = 0;
+            startTime = Time.timeSinceLevelLoad;
             if(!lookat) lookat = GameObject.Find("SM_Prop_Table_04").transform;
         }
 
@@ -39,7 +41,7 @@ namespace Game
                 // 更新倒计时文本
                 UpdateTimerText(RemainingTime);
 
-                if (ElapsedTime >= 237) // 240秒 = 4分钟
+                if (ElapsedTime >= 240) // 240秒 = 4分钟
                 {
                     if (!isFinalBattle)
                     {
@@ -61,7 +63,7 @@ namespace Game
         private void UpdateTimerText(float remainingTime)
         {
             int minutes = Mathf.FloorToInt((remainingTime - 60) / 60);
-            int seconds = Mathf.FloorToInt((remainingTime - 60) / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
 
             if (remainingTime >= 60)
             {
@@ -80,9 +82,10 @@ namespace Game
             ParticleSystem transfer = Resources.Load<ParticleSystem>("Liberate_04.1_Darkness");
             if (transfer == null) Debug.LogError("NO transfer");
             var transferi = new ParticleSystem[8];
+            PlayerController playerController = PlayerController.Instance;
             for (int x = 0; x < 8; ++x)
             {
-                transferi[x] = Instantiate(transfer, PlayerController.Instance.transform.position, Quaternion.identity);
+                transferi[x] = Instantiate(transfer, playerController.transform.position, Quaternion.identity);
             }
             yield return new WaitForSeconds(2f);
             for (int x = 0; x < 8; ++x)
@@ -90,8 +93,23 @@ namespace Game
                 transferi[x].Stop();
                 Destroy(transferi[x].gameObject);
             }
-            PlayerController.Instance.transform.position = bossRoom.position + Vector3.up * 2f;
-            PlayerController.Instance.transform.forward =lookat.position - PlayerController.Instance.transform.position;
+            playerController.transform.position = bossRoom.position + Vector3.up * 2f;
+            // PlayerController.Instance.transform.forward =lookat.position - PlayerController.Instance.transform.position;
+            Vector3 targetDirection = lookat.transform.position - playerController.transform.position;
+            targetDirection.y = 0f; // 将Y轴分量置零，以确保只在水平面上旋转
+            float maxRotationAngle = 360f; // 调整最大旋转角度
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            // 旋转到目标方向
+            while (Quaternion.Angle(playerController.transform.rotation, targetRotation) > 0.1f)
+            {
+                // 计算旋转步长
+                float step = maxRotationAngle * Time.deltaTime;
+
+                // 使用RotateTowards旋转
+                playerController.transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
+                yield return null;
+            }
+            
             
             if(!BGM) BGM = GameObject.Find("BGM").GetComponent<AudioSource>();
             BGM.clip = Resources.Load<AudioClip>("Music/沙场");
